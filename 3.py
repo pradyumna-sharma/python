31df9f5b633653bb6d550c72311cd38c8f1b71a0
Pradyumna Sharma V S <132439196+pradyumna-sharma@users.noreply.github.com>
2023-11-10 17:17:37 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,3 @@
+import mod_reverse as rev
+rev.reverse1("pradyumna")
+rev.reverse1("pratham")
---
+++ mod_reverse.py
@@ -0,0 +1,7 @@
+def reverse1(word1):
+    s1=word1
+    len1=len(s1)
+    for i in range(1,len1+1,1):
+        print(s1[len1-i],end="")
+    print()
+
